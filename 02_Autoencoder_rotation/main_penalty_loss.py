critila error in the calculation of penalty loss fixes, positon of sum chnanged

02_Autoencoder_rotation/main_penalty_loss.py
@@ -46,7 +46,6 @@ def rotate_tensor(input,rot_range=np.pi,plot=False):
         rows=int(np.floor(N**0.5))
         cols=N//rows
 
-        if cols*rows!=N 
         plt.figure()
 
         for j in range(rows*cols):
@@ -174,8 +173,8 @@ class Penalty_Loss(nn.Module):
             dot_prod=torch.bmm(x_i.view(batch_size,1,2),y_i.view(batch_size,2,1)).view(batch_size,1)
             x_norm=torch.norm(x_i, p=2, dim=1, keepdim=True)
             y_norm=torch.norm(y_i, p=2, dim=1, keepdim=True)
-            import ipdb; ipdb.set_trace()
-            reg_loss+= (torch.sum(dot_prod/(x_norm*y_norm))-1)**2
+            reg_loss+=((dot_prod/(x_norm*y_norm)-1)**2).sum()
+            
         if self.size_average:
             reg_loss=reg_loss/x.shape[0]/(ndims//2)
         return reg_loss